c4bc051f64a059f304de077144973b53be67406f
Dosya <abdk4lyk0v@gmail.com>
2024-11-07 19:49:51 +0600

    - added booking service - modified docker-compose -

---
+++ services/booking/src/test/java/kg/com/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.com.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
